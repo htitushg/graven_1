43b439cae195fca870a137c6ea65c3413af99539
htitushg <htitushg@gmail.com>
2024-10-25 18:45:08 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+
+# Créer une première fenêtre
+window=Tk()
+# Personnaliser la fenêtre
+window.title("My application")
+window.geometry("10440x600")
+window.minsize(480,360)
+window.iconbitmap("Data/hg.ico")
+window.mainloop()
